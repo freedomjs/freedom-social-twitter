2d3001ff6d144f466c692f644230a0c81043c6b9
soycode <soycode@users.noreply.github.com>
2015-12-15 15:06:22 -0800

    initial npm and grunt

---
+++ Gruntfile.js
@@ -0,0 +1,82 @@
+/**
+ * Gruntfile for freedom-social-twitter
+ **/
+
+var path = require('path');
+
+module.exports = function(grunt) {
+  grunt.initConfig({
+    copy: {
+      build: {
+        cwd: 'src/',
+        src: ['**'],
+        dest: 'build/',
+        flatten: false,
+        filter: 'isFile',
+        expand: true
+      },
+      freedom: {
+        src: [ require.resolve('freedom') ],
+        dest: 'build/',
+        flatten: true,
+        filter: 'isFile',
+        expand: true,
+        onlyIf: 'modified'
+      }
+    },
+
+    browserify: {
+      twitter: {
+        files: {
+          'build/twitter.js': [ require.resolve('twitter-js') ]
+        },
+        options: {
+          browserifyOptions: {
+            standalone: 'twitter'
+          }
+        }
+      }
+    },
+
+    jshint: {
+      all: ['src/**/*.js', 'spec/**/*.js'],
+      options: {
+        jshintrc: true
+      }
+    },
+
+    // TODO make client demo
+    /*connect: {
+      demo: {
+      options: {
+      port: 8000,
+      keepalive: true,
+      base: ['./', 'build/'],
+      open: 'http://localhost:8000/build/'
+      }
+      }
+      },*/
+
+    clean: ['build/']
+  });
+
+  grunt.loadNpmTasks('grunt-browserify');
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  //grunt.loadNpmTasks('grunt-contrib-connect');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+
+  grunt.registerTask('build', [
+    'jshint',
+    'copy',
+    'browserify'
+  ]);
+  grunt.registerTask('demo', [
+    'build',
+    //'connect'
+  ]);
+  grunt.registerTask('default', [
+    'build'
+  ]);
+
+}
